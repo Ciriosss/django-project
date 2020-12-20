0b641909ad0e8fd62fcaf23dfbd064b3ec97f8ca
Domenico Ciriello <domenicociriello2000@gmail.com>
2020-12-20 16:58:12 +0100

    Ip control

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from datetime import datetime
 from users.models import Profile
 from django.contrib.auth.models import User
+
 from django.views.decorators.cache import cache_page
 from django.conf import settings
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
@@ -38,6 +39,7 @@ def checkIp(request):
 CACHE_TTL = getattr(settings, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 # home views
+@login_required
 @cache_page(CACHE_TTL)
 def home(request):
     checkIp(request)
